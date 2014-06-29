21e0a8b5507264ad0f1c20fe29106404c26b1198
Jeffrey <jeffrey.elam@centre.edu>
2014-06-29 15:35:21 -0400

    Updated calendar and home page

--- 1_0/Calendar/calendar_jquery.js
+++ 1_0/Calendar/calendar_jquery.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
 //Functions go in here...
+$('#calendar').fullCalendar({
+				header: {
+					left: 'prev,next today',
+					center: 'title',
+					right: 'month,agendaWeek,agendaDay'
+				},
+				defaultDate: '2014-06-12',
+				editable: true,
+				events: [
+					/*Events get added here...*/
+				]
+			});
 
 });
